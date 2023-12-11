7d7ed97ddf74fdf5e0594e4259f08ed580332d30
Azamat2004 <kokosherovazamat04@gmail.com>
2023-12-11 17:10:06 +0600

    first

---
+++ App.js
@@ -0,0 +1,51 @@
+import Aza from "./Aza";
+import { useState } from "react";
+
+function App() {
+  const [num, setNum] = useState(0);
+
+  const handleDecrement = () => {
+    if (num > 0) {
+      setNum(num - 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1>{num}{num == 15 ? 'max' : num == 0 ? 'min' : ''}
+      </h1>
+
+      <button onClick={() => {
+        if (num < 15) {
+          setNum(num + 1);
+        }
+      }
+      }
+
+
+      >+</button>
+
+      <button onClick={()=>{
+          if (num+5 <= 15) {
+            setNum(num + 5)
+          }
+      }}>+5</button>
+      <button  onClick={()=>{
+        if (num-5 >=0) {
+           setNum(num - 5)
+        }
+
+           
+          
+      }}>-5</button>
+      <button onClick={handleDecrement}>-</button>
+
+      <button onClick={() => setNum(0)}>reset</button>
+
+      {/* <Home />
+     <Aza /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ Aza.jsx
@@ -0,0 +1,10 @@
+
+const Aza = () => {
+    return (
+        <div>
+            <h1>bvuvbbbfoinfnwugniutb</h1>
+        </div>
+    );
+}
+
+export default Aza;
